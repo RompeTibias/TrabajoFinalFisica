4bff8ab625e62a912ec41d5c31f2496fc9594d73
Santiep9 <santiestrela9@gmail.com>
2026-05-31 23:57:24 +0200

    levele tre (1+1+1)

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,12 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private string namee;
     [SerializeField] private GameObject target;
+
+    [SerializeField] private Transform target2;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float velocidad = 2f;
+
+    private Vector3 posicionInicial;
+    private Vector3 posicionFinal;
+    private bool movida = false;
+    private bool moviendo = false;
+
     void Start()
     {
-        
+        posicionInicial = target2.position;
+        posicionFinal = posicionInicial + offset;
     }
 
     void Update()
@@ -17,6 +29,7 @@ public class Interactable : MonoBehaviour
     public void Interact()
     {
         Interaxtion(namee);
+        MoverPlataforma();
     }
 
     void Interaxtion(string name)
@@ -51,5 +64,34 @@ public class Interactable : MonoBehaviour
                 Debug.Log("Nothing to interact with");
                 break;
         }
+    }
+
+    public void MoverPlataforma()
+    {
+        if (moviendo) return;
+
+        Vector3 destino = movida ? posicionInicial : posicionFinal;
+        StartCoroutine(Mover(destino));
+
+        movida = !movida;
+    }
+
+    private IEnumerator Mover(Vector3 destino)
+    {
+        moviendo = true;
+
+        while (Vector3.Distance(target2.position, destino) > 0.01f)
+        {
+            target2.position = Vector3.MoveTowards(
+                target2.position,
+                destino,
+                velocidad * Time.deltaTime
+            );
+
+            yield return null;
+        }
+
+        target2.position = destino;
+        moviendo = false;
     }
 }
---
+++ Assets/Scripts/PlataformaMovil.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlataformaMovil : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
